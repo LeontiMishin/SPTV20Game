f2216d1ba935ba97c871634026a90eeb0b3bece9
Your Name <you@example.com>
2021-09-21 14:36:27 +0300

    Циклы

--- src/sptv20game/SPTV20Game.java
+++ src/sptv20game/SPTV20Game.java
@@ -1,40 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sptv20game;
 
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- *
- * @author User
- */
 public class SPTV20Game {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // Программа загадывает число и просит пользователя отгадать
-        int max = 5, min = 0;
-        Random random = new Random();
-        int gamenum = random.nextInt(max-min+1)+min;
-        
         System.out.println("Программа загадала число от 0 до 5, угадай: ");
-        Scanner scanner = new Scanner(System.in);
-        int usernum = scanner.nextInt();
-        
-        if(gamenum == usernum){
-            System.out.println("Ура! Ты выиграл!");
-        }else{
-            System.out.println(" ;) Ты проиграл. Задумано число: " + gamenum);
-            
+        int max = 5, min = 0, attempt = 1;
+        Random random = new Random();
+        int gameNum = random.nextInt(max-min+1)+min;
+        for(int i=0;i<3;i++){
+            Scanner scanner = new Scanner(System.in);
+            int userNum = scanner.nextInt();
+            if(gameNum == userNum){
+                System.out.println("Ура! Ты выиграл!");
+                break;
+            }else{
+                if(attempt < 3) {
+                    System.out.println("Неправильно, поробуй ещё раз: ");
+                }else{
+                    System.out.println(" ;) Ты проиграл. Задумано число: " + gameNum);    
+                }    
+            }
+            attempt++;
         }
-        
-                
     }
-    
 }
